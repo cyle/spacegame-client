d194df484b909a7f3e4318dd6f053d2b84617a07
cyle <cyle@standingdesk3>
2013-08-23 13:20:36 -0400

    - now keep the latest BABYLON files in the repo, for easier version maintenance

---
+++ Babylon/Culling/Octrees/babylon.octree.js
@@ -0,0 +1,45 @@
+﻿var BABYLON = BABYLON || {};
+
+(function() {
+    BABYLON.Octree = function(maxBlockCapacity) {
+        this.blocks = [];
+        this._maxBlockCapacity = maxBlockCapacity || 64;
+        this._selection = new BABYLON.Tools.SmartArray(256);
+    };
+
+    // Methods
+    BABYLON.Octree.prototype.update = function(worldMin, worldMax, meshes) {
+        BABYLON.Octree._CreateBlocks(worldMin, worldMax, meshes, this._maxBlockCapacity, this);
+    };
+
+    BABYLON.Octree.prototype.select = function(frustumPlanes) {
+        this._selection.reset();
+
+        for (var index = 0; index < this.blocks.length; index++) {
+            var block = this.blocks[index];
+            block.select(frustumPlanes, this._selection);
+        }
+
+        return this._selection;
+    };
+
+    // Statics
+    BABYLON.Octree._CreateBlocks = function (worldMin, worldMax, meshes, maxBlockCapacity, target) {
+        target.blocks = [];
+        var blockSize = new BABYLON.Vector3((worldMax.x - worldMin.x) / 2, (worldMax.y - worldMin.y) / 2, (worldMax.z - worldMin.z) / 2);
+
+        // Segmenting space
+        for (var x = 0; x < 2; x++) {
+            for (var y = 0; y < 2; y++) {
+                for (var z = 0; z < 2; z++) {
+                    var localMin = worldMin.add(blockSize.multiplyByFloats(x, y, z));
+                    var localMax = worldMin.add(blockSize.multiplyByFloats(x + 1, y + 1, z + 1));
+
+                    var block = new BABYLON.OctreeBlock(localMin, localMax, maxBlockCapacity);
+                    block.addEntries(meshes);
+                    target.blocks.push(block);
+                }
+            }
+        }
+    };
+})();
---
+++ Babylon/Culling/Octrees/babylon.octreeBlock.js
@@ -0,0 +1,72 @@
+﻿var BABYLON = BABYLON || {};
+
+(function () {
+    BABYLON.OctreeBlock = function (minPoint, maxPoint, capacity) {
+        this.subMeshes = [];
+        this.meshes = [];
+        this._capacity = capacity;
+
+        this._minPoint = minPoint;
+        this._maxPoint = maxPoint;
+        
+        this._boundingVectors = [];
+
+        this._boundingVectors.push(minPoint.clone());
+        this._boundingVectors.push(maxPoint.clone());
+
+        this._boundingVectors.push(minPoint.clone());
+        this._boundingVectors[2].x = maxPoint.x;
+
+        this._boundingVectors.push(minPoint.clone());
+        this._boundingVectors[3].y = maxPoint.y;
+
+        this._boundingVectors.push(minPoint.clone());
+        this._boundingVectors[4].z = maxPoint.z;
+
+        this._boundingVectors.push(maxPoint.clone());
+        this._boundingVectors[5].z = minPoint.z;
+
+        this._boundingVectors.push(maxPoint.clone());
+        this._boundingVectors[6].x = minPoint.x;
+
+        this._boundingVectors.push(maxPoint.clone());
+        this._boundingVectors[7].y = minPoint.y;
+    };
+
+    // Methods
+    BABYLON.OctreeBlock.prototype.addEntries = function (meshes) {
+        for (var index = 0; index < meshes.length; index++) {
+            var mesh = meshes[index];
+
+            if (mesh.getBoundingInfo().boundingBox.intersectsMinMax(this._minPoint, this._maxPoint)) {
+                var localMeshIndex = this.meshes.length;
+                this.meshes.push(mesh);
+
+                this.subMeshes[localMeshIndex] = [];
+                for (var subIndex = 0; subIndex < mesh.subMeshes.length; subIndex++) {
+                    var subMesh = mesh.subMeshes[subIndex];
+                    if (mesh.subMeshes.length === 1 || subMesh.getBoundingInfo().boundingBox.intersectsMinMax(this._minPoint, this._maxPoint)) {
+                        this.subMeshes[localMeshIndex].push(subMesh);
+                    }
+                }
+            }
+        }
+        
+        if (this.subMeshes.length > this._capacity) {
+            BABYLON.Octree._CreateBlocks(this._minPoint, this._maxPoint, this.meshes, this._capacity, this);
+        }
+    };
+
+    BABYLON.OctreeBlock.prototype.select = function (frustumPlanes, selection) {
+        if (this.blocks) {
+            for (var index = 0; index < this.blocks.length; index++) {
+                var block = this.blocks[index];
+                block.select(frustumPlanes, selection);
+            }
+            return;
+        }
+        if (BABYLON.BoundingBox.IsInFrustrum(this._boundingVectors, frustumPlanes)) {
+            selection.push(this);
+        }
+    };
+})();
