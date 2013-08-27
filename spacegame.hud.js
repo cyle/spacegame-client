fa55a91233ddf7c2ae43fe366ce6291854487c5e
Cyle <cylegage@gmail.com>
2013-08-26 22:33:21 -0400

    - added dummy hit points to HUD

--- spacegame.hud.js
+++ spacegame.hud.js
@@ -16,6 +16,7 @@ setInterval(function() {
 	hud_html += '<td>'+prettyNum(radiansToDegrees(playerShip.currentRotation), 2)+'&deg; / '+prettyNum(radiansToDegrees(playerShip.movingRotation), 2)+'&deg;</td>';
 	hud_html += '<td>'+prettyNum(playerShip.currentSpeed * 500, 2)+' m/s</td>';
 	hud_html += '<td>0.0e / 10.0e</td>';
+	hud_html += '<td>50 / 50 hp</td>';
 	
 	hud_html += '</tr>';
 	hud_html += '</table>';
